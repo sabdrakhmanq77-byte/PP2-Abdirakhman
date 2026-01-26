bd313ab875a5c46e33ff186d2a8adf6725021bc7
Abdirakhman <sabdrakhmanq77@gmail.com>
2026-01-26 21:55:41 +0500

    added types of modifying

--- PP2/pr1/python-basics/string.py
+++ PP2/pr1/python-basics/string.py
@@ -3,3 +3,9 @@ print(s[2:5]) #slicing
 print(s[:5]) #print literal till index 5
 print(s[2:]) #print from index 2 to end
 print(s[-5:-2]) #negative indexing
+print(s.upper()) #convert to uppercase
+print(s.lower()) #convert to lowercase
+print(s.strip()) #remove whitespace
+print(len(s)) #length of string
+print(s.replace("H","J")) #replace H with J
+print(s.split(",")) #split string at comma
