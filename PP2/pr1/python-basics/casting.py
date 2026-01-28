06214b38cb9451b8e747b856ba8c7e5fa25c154e
Abdirakhman <sabdrakhmanq77@gmail.com>
2026-01-28 20:15:05 +0500

    explaining for didara

--- PP2/pr1/python-basics/casting.py
+++ PP2/pr1/python-basics/casting.py
@@ -1,12 +1,18 @@
-x=1
-y=2.8
-z='3'
+# x=1
+# y=2.8
+# z='3'
 
-x=float(x)
-y=float(y)
-z=float(z)
+# x=float(x)
+# y=float(y)
+# z=float(z)
 
-x=str(x)
-y=str(y)
-z=str(z)
+# x=str(x)
+# y=str(y)
+# z=str(z)
 
+# num=int(input(''))
+# print(num*5)
+
+name=input('')
+snam=input('')
+print(f"Hello, {name} {snam}")
