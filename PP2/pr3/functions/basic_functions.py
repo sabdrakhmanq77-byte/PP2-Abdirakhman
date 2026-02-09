955508375e22d930d4162788f8e024dea6ab4497
Abdirakhman <sabdrakhmanq77@gmail.com>
2026-02-09 19:30:08 +0500

    pr3 functions part1

---
+++ PP2/pr3/functions/basic_functions.py
@@ -0,0 +1,24 @@
+# A function is a block of code which only runs when it is called.
+
+# A function can return data as a result.
+
+# A function helps avoiding code repetition.
+
+def hello_W():
+    print("Hello, World!")
+
+# hello_W() calling for function
+
+hello_W()
+
+# Functions needed for shorttening code
+
+def pow_two(x):
+    return x ** 2
+a=3
+print(pow_two(a))
+b=5
+print(pow_two(b))
+
+# i called for function for a and b instead of writing a**2 and b**2 which is more efficient and short.
+
---
+++ PP2/pr3/functions/function-arguments.py
@@ -0,0 +1,8 @@
+def greet(name):  
+    print(f"Hello, {name}!")
+
+greet("Alice") #Hello, Alice!
+greet("Bob") #Hello, Bob!
+
+def add(a, b): #parameters
+    print(a + b) #arguments
