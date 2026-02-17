02f105a9255368c1abed87bc5b7af9500f6d1eb7
Abdirakhman <sabdrakhmanq77@gmail.com>
2026-02-17 11:37:35 +0500

    fixed problem

--- PP2/pr3/inheritance/super_function.py
+++ PP2/pr3/inheritance/super_function.py
@@ -1,3 +1,7 @@
+class Person:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
 class Student(Person):
   def __init__(self, fname, lname):
     super().__init__(fname, lname)
