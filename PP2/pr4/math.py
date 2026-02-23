45e2eb8db3a6a238da5432e3279b85b8f859758b
Abdirakhman <sabdrakhmanq77@gmail.com>
2026-02-23 22:03:02 +0500

    math

---
+++ PP2/pr4/math.py
@@ -0,0 +1,21 @@
+import math
+import random
+
+x = min(5, 10, 25)
+y = max(5, 10, 25)
+z = abs(-7.25)
+a = pow(4, 3)
+b = math.sqrt(64)
+c = math.ceil(1.4)
+d = math.floor(1.4)
+e= math.pi
+f=random.randint(1, 10)
+print(e)
+print(f)
+print(c)
+print(d) 
+print(b)
+print(a)
+print(z)
+print(x)
+print(y)
