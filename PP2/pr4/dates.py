c34a75298169fc18f79a29075bbde0a3f04d854a
Abdirakhman <sabdrakhmanq77@gmail.com>
2026-02-23 21:48:44 +0500

    dates

---
+++ PP2/pr4/dates.py
@@ -0,0 +1,14 @@
+import datetime
+
+x = datetime.datetime.now()
+print(x)
+print(x.year)
+print(x.strftime("%A")) #%A- format
+y = datetime.datetime(2020, 5, 17)
+print(y)    
+# %a	Weekday, short version
+# %A	Weekday, full version
+# %b	Month name, short version  
+# %B	Month name, full version
+
+
