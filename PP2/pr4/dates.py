9dc3738e75b780f389b99ef06be549483ef860b3
Abdirakhman <sabdrakhmanq77@gmail.com>
2026-03-02 20:42:19 +0500

    oi

--- PP2/pr4/dates.py
+++ PP2/pr4/dates.py
@@ -1,14 +1,32 @@
-import datetime
+# import datetime
 
-x = datetime.datetime.now()
-print(x)
-print(x.year)
-print(x.strftime("%A")) #%A- format
-y = datetime.datetime(2020, 5, 17)
-print(y)    
+# x = datetime.datetime.now()
+# print(x)
+# print(x.year)
+# print(x.strftime("%A")) #%A- format
+# y = datetime.datetime(2020, 5, 17)
+# print(y)    
 # %a	Weekday, short version
 # %A	Weekday, full version
 # %b	Month name, short version  
 # %B	Month name, full version
 
+import datetime
 
+today=datetime.date.today()
+result=today-datetime.timedelta(days=5)
+print(result)
+
+yesterday=today-datetime.timedelta(days=1)
+tommorow=today+datetime.timedelta(days=1)
+print( yesterday)
+print(tommorow)
+
+now=datetime.datetime.now()
+new=now.replace(microsecond=0)
+print(new)
+
+fd=datetime.datetime(2020, 5, 17)
+sd=datetime.datetime(2021, 6, 18)
+diff=sd-fd
+print(diff)
--- PP2/pr4/generators.py
+++ PP2/pr4/generators.py
@@ -1,43 +1,80 @@
-#Lists, tuples, dictionaries, and sets are all iterable objects. They are iterable containers which you can get an iterator from.
+# #Lists, tuples, dictionaries, and sets are all iterable objects. They are iterable containers which you can get an iterator from.
 
-mytuple = ("apple", "banana", "cherry")
-myit = iter(mytuple)
+# mytuple = ("apple", "banana", "cherry")
+# myit = iter(mytuple)
 
-print(next(myit))
-print(next(myit))
-print(next(myit))
+# print(next(myit))
+# print(next(myit))
+# print(next(myit))
 
-for x in mytuple:
-  print(x)
+# for x in mytuple:
+#   print(x)
 
-class MyNumbers:     #make iterator
-  def __iter__(self):
-    self.a = 1
-    return self
+# class MyNumbers:     #make iterator
+#   def __iter__(self):
+#     self.a = 1
+#     return self
 
-  def __next__(self): #male next method
-    if self.a <= 20:
-      x = self.a
-      self.a += 1
-      return x
-    else:
-      raise StopIteration # by setting a condition we can make the stopiterator
+#   def __next__(self): #made next method
+#     if self.a <= 20:
+#       x = self.a
+#       self.a += 1
+#       return x
+#     else:
+#       raise StopIteration # by setting a condition we can make the stopiterator
 
-myclass = MyNumbers()
-myiter = iter(myclass)
+# myclass = MyNumbers()
+# myiter = iter(myclass)
 
-for x in myiter:
-  print(x)
+# for x in myiter:
+#   print(x)
 
-#creating generator object
-squarenum=(x*x for x in range(10))
-for i in squarenum:
+# #creating generator object
+# squarenum=(x*x for x in range(10))
+# for i in squarenum:
+#     print(i)
+# #generators as functions
+# def my_gen():
+#     n=0
+#     while n<10:
+#         yield n
+#         n+=1
+# for i in my_gen():
+#     print(i)
+
+N=int(input(''))
+square=(x**2 for x in range(N))
+for i in square:
     print(i)
-#generators as functions
-def my_gen():
-    n=0
-    while n<10:
+
+n=int(input(''))
+def my_gen(n):
+    for i in range(n):
+        if i%2==0:
+            yield i
+print(",".join(str(i) for i in my_gen(n)))
+
+def divisible_by_3_and_4(n):
+    for i in range(n + 1):
+        if i % 3 == 0 and i % 4 == 0:
+            yield i
+
+for number in divisible_by_3_and_4(n):
+    print(number)
+
+a=int(input(''))
+b=int(input(''))
+def squares(a, b):
+    for i in range(a, b + 1):
+        yield i ** 2
+
+for value in squares(1, 5):
+    print(value)
+
+def countdown(n):
+    while n >= 0:
         yield n
-        n+=1
-for i in my_gen():
-    print(i)
+        n -= 1
+
+for number in countdown(5):
+    print(number)
